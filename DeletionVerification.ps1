fb715c4aea554c81c03c91aa7e74498948f83246
EzekialPage <39393643+EzekialPage@users.noreply.github.com>
2020-10-12 20:44:56 -0500

    Add files via upload

---
+++ DeletionVerification.ps1
@@ -0,0 +1,15 @@
+﻿#validate deletion of encoding line at top of xml file
+Write-Output "Second part:"
+
+$noEncodingLine = Get-ChildItem -Path C:\Users\Family\Desktop\TempDCT\*.xml | Where-Object {!($_ | Select-String 'encoding="utf-8' -Quiet) }
+$encodingLine = Get-ChildItem -Path C:\Users\Family\Desktop\TempDCT\*.xml | Select-String -Pattern 'encoding="utf-8"'
+if ($encodingLine -eq $null) {
+    "No encoding line found in the files"
+} else {
+    "Encoding line found in the following files: "
+    $encodingLine | foreach {$_.FileName}
+    Write-Output " "
+    "No encoding line found in the following files: "
+    $noEncodingLine | foreach {$_.Name}
+
+}
